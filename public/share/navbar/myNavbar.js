7dba687be89210bc40bd4d5c516a48ebe91039f5
parkersruth <psr23@uw.edu>
2023-01-07 23:36:55 -0800

    adding publications section

--- public/share/navbar/myNavbar.js
+++ public/share/navbar/myNavbar.js
@@ -7,7 +7,7 @@ document.getElementById("myNavbar").innerHTML =
       '<ul class="navbar-nav">'+
         '<li><a class="mylink" href="/">home</a></li>'+
         '<li><a class="mylink" href="/#project">projects</a></li>'+
-        //'<li><a class="mylink" href="/#tab2">publication</a></li>'+
+        '<li><a class="mylink" href="/#publications">publications</a></li>'+
         '<li><a class="mylink" href="/#people">people</a></li>'+
         //'<li><a class="mylink" href="/#tab4">sponsors</a></li>'+
       '</ul>'+
